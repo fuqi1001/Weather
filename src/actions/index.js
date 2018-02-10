480e92644f90369e6671e75d798d22abefd40042
qifu <fuqiqi12345@gmail.com>
2018-02-10 15:48:22 -0500

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,9 +6,12 @@ const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast`
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
-    console.log(city)
+    //console.log(city)
     const url = `${ROOT_URL}?q=${city},us&appid=${API_KEY}`;
     const request = axios.get(url);
+
+    console.log('Request: ', request);
+    
     return {
         type: FETCH_WEATHER,
         payload: request
