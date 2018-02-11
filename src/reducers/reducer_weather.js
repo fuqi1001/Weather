7dd10c73851d3d415a41f75582d10f825ddf452e
qifu <fuqiqi12345@gmail.com>
2018-02-10 22:06:06 -0500

    updated

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,13 +1,13 @@
-import FETCH_WEATHER from '../actions/index'
+import { FETCH_WEATHER } from '../actions/index'
 
-export default function(state = [], action) {
-   //console.log('Action received', action);
-   switch(action.type) {
-       case FETCH_WEATHER:
-        //return state.concat([action.payload.data]);
-        //ES6 way
-        return [action.payload.data, ...state];
-        
-   }
-   return state;
+export default function (state = [], action) {
+    //console.log('Action received', action);
+    switch (action.type) {
+        case FETCH_WEATHER:
+            //return state.concat([action.payload.data]);
+            //ES6 way
+            return [action.payload.data, ...state];
+
+    }
+    return state;
 }
