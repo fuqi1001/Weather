9342609b02d52df1ff94307b14efcc235de02a13
qifu <fuqiqi12345@gmail.com>
2018-02-10 23:49:46 -0500

    add chart

---
+++ src/components/chart.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    Sparklines,
+    SparklinesLine,
+    SparklinesReferenceLine
+} from "react-sparklines";
+import _ from 'lodash'
+
+function average(data) {
+    return _.round(_.sum(data) / data.length);
+}
+
+export default props => {
+    return (
+        <div>
+            <Sparklines svgHeight={160} svgWidth={360} data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>
+        </div>
+    );
+};
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Chart from '../components/chart'
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
         return (
-            <tr key= {name}>
+            <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressure} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         )
     }
@@ -16,9 +24,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,7 +45,7 @@ function mapStateToProps(state) {
 ES6
 function mapStateToProps(weather) {
     return {
-        weather : weather
+                    weather : weather
     }
 }
 */
